b0f803f4bd55226ef8e233ca61952af433f47ac4
siddharrth <standuka@masonlive.gmu.edu>
2020-02-19 11:23:50 -0500

    second commit

---
+++ src/main/java/abc/xy/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package abc.xy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+	
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String showHelloPage() {
+		System.out.println("Hello from controller");
+		return "hello-page";
+		
+	}
+
+}
---
+++ src/main/webapp/pages/hello-page.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2> I am Great. I am coming from hello controller</h2>
+</body>
+</html>
